498d7a0f1f0395a48488e25472eae37325e1d6ce
LeXini <marcos_biazus@hotmail.com>
2022-03-09 20:48:47 -0300

    config correcao

--- banco.sql
+++ banco.sql
@@ -4,8 +4,8 @@ create table equipe (
 codigo serial not null primary key, 
 nome varchar(50) not null, 
 fundacao integer not null, 
-esporte varchar(50) not null
+esporte varchar(50) not null,
 estado varchar(50) not null);
 
 -- inserir alguns registros
-insert into equipe (nome, fundacao, esporte) values ('gremio', 1903, 'futebol', 'rs'), ('internacional', 1909, 'futebol', 'rs');
+insert into equipe (nome, fundacao, esporte, estado) values ('gremio', 1903, 'futebol', 'rs'), ('internacional', 1909, 'futebol', 'rs');
